274370b0d07cc91325a9c18a5a4185dd4f663f59
Vamshi <vamshivardhanemmadi@gmail.com>
2024-04-08 11:31:39 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import { Mongoose } from "mongoose";
+
+const userSchema =new Mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+},{timestamps:true}
+);
+const User=mongoose.model('User',userSchema);
+
+export default User;
