5d7f712687f67e9a1e13e2c7ce04022c56096e48
Vamshi <vamshivardhanemmadi@gmail.com>
2024-04-11 20:01:07 +0530

    comment section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -13,10 +12,12 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -10,7 +10,7 @@ export default function CallToAction() {
             <p className='text-gray-500 my-2'>
                 Checkout these resources with 100 JavaScript Projects
             </p>
-            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+            <Button color='purple' className='rounded-tl-xl rounded-bl-none'>
                 <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
                     100 JavaScript Projects
                 </a>
