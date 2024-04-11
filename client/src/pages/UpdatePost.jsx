db8701995bf13161dd6c9d12b647ab1ed382dfcb
Vamshi <vamshivardhanemmadi@gmail.com>
2024-04-11 18:09:33 +0530

    small theme changes in the update post page

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -135,7 +135,7 @@ export default function UpdatePost() {
             <option value='nextjs'>Next.js</option>
           </Select>
         </div>
-        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+        <div className='flex gap-4 items-center justify-between border-2 border-slate-300 p-3'>
           <FileInput
             type='file'
             accept='image/*'
@@ -145,7 +145,6 @@ export default function UpdatePost() {
             type='button'
             gradientDuoTone='purpleToBlue'
             size='sm'
-            outline
             onClick={handleUpdloadImage}
             disabled={imageUploadProgress}
           >
@@ -179,7 +178,7 @@ export default function UpdatePost() {
             setFormData({ ...formData, content: value });
           }}
         />
-        <Button type='submit' gradientDuoTone='purpleToPink'>
+        <Button type='submit' color='purple'>
           Update post
         </Button>
         {publishError && (
