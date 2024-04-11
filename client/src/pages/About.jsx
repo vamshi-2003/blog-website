e8f7954ac7426fd5fe16b7e2fe6d7444394753c0
Vamshi <vamshivardhanemmadi@gmail.com>
2024-04-11 20:47:27 +0530

    search page,home page,about and projects added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            MERN BLOG
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              This is just a website made for an assignment which can online for full time in future
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
